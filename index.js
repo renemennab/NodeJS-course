1967c9ef415b30f959df4ada6a8edfa1aa4e85da
Rene Boaventura <rene.boaventura@intelie.com.br>
2021-03-20 17:26:31 -0300

    get query params

--- index.js
+++ index.js
@@ -8,17 +8,26 @@ const http = require('http')
 // The server should respond to all requests with a string
 const server = http.createServer((req, res)=> {
 
+    // Get parsed url obj
+    const url = new URL(req.url, `http://${req.headers.host}`)
+
     // Get the path from the url
-    const trimmedPath = req.url.replace(/^\/+|\/+$/g, '')
+    const path = url.pathname
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
 
     // Get the HTTP method
     const method = req.method.toLowerCase()
+
+    // Get query params
+    const params = url.searchParams
 
     // Send the response
     res.end('Hello World\n')
 
     // Log the request path
-    console.log('Reques received on path: ', trimmedPath, 'with this method: ', method)
+    console.log('Reques received on path:', trimmedPath)
+    console.log('Request method:', method)
+    console.log('Query params:', params)
 
 })
 
